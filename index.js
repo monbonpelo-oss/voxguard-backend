55d6c229004d716bee1e419f9017c6a9ac71b36e
monbonpelo-oss <monbonpelo@gmail.com>
2026-02-22 00:56:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("VoxGuard backend is running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "API working",
+    timestamp: new Date()
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
