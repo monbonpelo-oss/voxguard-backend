f46af2016b551f7cb0d6efecce5b62eb121fc14a
stark <stark@Mac-mini-de-stark.local>
2026-02-22 04:04:38 +0100

    fix render port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("VoxGuard backend is running");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000");
 });
