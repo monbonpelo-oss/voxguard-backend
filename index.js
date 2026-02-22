ad5e821a7624eee4a9eb014af7d6789d3e710dbb
monbonpelo-oss <monbonpelo@gmail.com>
2026-02-22 01:39:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ app.use(express.json());
 const healthRoute = require("./routes/health");
 app.use("/api/health", healthRoute);
 
+const usersRoute = require("./routes/users");
+app.use("/api/users", usersRoute);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
-});
+});”
